0fe4bb23116446a40bee014a6b984f5244e3b869
Benjamin Bengfort <benjamin@bengfort.com>
2022-04-06 16:49:01 -0500

    rebuild script

--- containers/rvasp/rebuild.sh
+++ containers/rvasp/rebuild.sh
@@ -1,13 +1,78 @@
 #!/bin/bash
 
-# Get the path to the project no matter where we're running this script from
-PROJECT=$(realpath $(dirname $(realpath $0))/..)
-CONTAINERS=$PROJECT/containers
+ask() {
+    local prompt default reply
+
+    if [[ ${2:-} = 'Y' ]]; then
+        prompt='Y/n'
+        default='Y'
+    elif [[ ${2:-} = 'N' ]]; then
+        prompt='y/N'
+        default='N'
+    else
+        prompt='y/n'
+        default=''
+    fi
+
+    while true; do
+
+        # Ask the question (not using "read -p" as it uses stderr not stdout)
+        echo -n "$1 [$prompt] "
+
+        # Read the answer (use /dev/tty in case stdin is redirected from somewhere else)
+        read -r reply </dev/tty
+
+        # Default?
+        if [[ -z $reply ]]; then
+            reply=$default
+        fi
+
+        # Check if the reply is valid
+        case "$reply" in
+            Y*|y*) return 0 ;;
+            N*|n*) return 1 ;;
+        esac
+
+    done
+}
+
+# Get the tag as the first argument or from git if none is supplied
+if [ -z "$1" ]; then
+    TAG=$(git rev-parse --short HEAD)
+else
+    TAG=$1
+fi
+
+# Confirm that we're continuing with the tag
+if ! ask "Continue with tag $TAG?" N; then
+    exit 1
+fi
+
+# Set some helpful variables
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+REPO=$(realpath "$DIR/../..")
+CONTAINERS=$REPO/containers/rvasp
 
 # Build the base rvasp image
-docker build -t trisacrypto/rvasp:latest -f $PROJECT/Dockerfile $PROJECT
+docker build --platform linux/amd64 -t trisa/rvasp:$TAG -f $CONTAINERS/Dockerfile $REPO
 
 # Build the alice, bob, and evil images
-docker build -t trisacrypto/rvasp:alice -f $CONTAINERS/alice/Dockerfile $PROJECT
-docker build -t trisacrypto/rvasp:bob -f $CONTAINERS/bob/Dockerfile $PROJECT
-docker build -t trisacrypto/rvasp:evil -f $CONTAINERS/evil/Dockerfile $PROJECT
+docker build --platform linux/amd64 -t trisa/rvasp-alice:$TAG  -f $CONTAINERS/alice/Dockerfile --build-arg BASE=trisa/rvasp:$TAG $REPO
+docker build --platform linux/amd64 -t trisa/rvasp-bob:$TAG -f $CONTAINERS/bob/Dockerfile --build-arg BASE=trisa/rvasp:$TAG $REPO
+docker build --platform linux/amd64 -t trisa/rvasp-evil:$TAG -f $CONTAINERS/evil/Dockerfile --build-arg BASE=trisa/rvasp:$TAG $REPO
+
+# Retag the alice, bob, and evil images
+docker tag trisa/rvasp:$TAG gcr.io/trisa-gds/rvasp:$TAG
+docker tag trisa/rvasp-alice:$TAG gcr.io/trisa-gds/rvasp-alice:$TAG
+docker tag trisa/rvasp-bob:$TAG gcr.io/trisa-gds/rvasp-bob:$TAG
+docker tag trisa/rvasp-evil:$TAG gcr.io/trisa-gds/rvasp-evil:$TAG
+
+docker push trisa/rvasp:$TAG
+docker push trisa/rvasp-alice:$TAG
+docker push trisa/rvasp-bob:$TAG
+docker push trisa/rvasp-evil:$TAG
+
+docker push gcr.io/trisa-gds/rvasp:$TAG
+docker push gcr.io/trisa-gds/rvasp-alice:$TAG
+docker push gcr.io/trisa-gds/rvasp-bob:$TAG
+docker push gcr.io/trisa-gds/rvasp-evil:$TAG
