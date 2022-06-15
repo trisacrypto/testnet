df3543eec76624b9836d56a2cd384d09abe0b28f
Benjamin Bengfort <benjamin@bengfort.com>
2022-06-15 14:17:35 -0500

    async handler panic recovery

--- pkg/rvasp/async.go
+++ pkg/rvasp/async.go
@@ -1,9 +1,12 @@
 package rvasp
 
 import (
+	"context"
 	"fmt"
+	"runtime/debug"
 	"time"
 
+	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"github.com/trisacrypto/testnet/pkg/rvasp/db"
 	pb "github.com/trisacrypto/testnet/pkg/rvasp/pb/v1"
@@ -13,8 +16,16 @@ import (
 // rVASP database and initiates TRISA transfers back to the originator. This allows the
 // rVASPs to simulate asynchronous transactions.
 func (s *TRISA) AsyncHandler(stop <-chan struct{}) {
-	ticker := time.NewTicker(s.parent.conf.AsyncInterval)
+	var (
+		ctx    context.Context
+		cancel context.CancelFunc
+	)
 
+	// Create default context and cancel to stop async handler routine before a handler is fired.
+	_, cancel = context.WithCancel(context.Background())
+
+	// Create the ticker to run aync handlers periodically
+	ticker := time.NewTicker(s.parent.conf.AsyncInterval)
 	log.Info().Dur("interval", s.parent.conf.AsyncInterval).Msg("asynchronous handler started")
 
 	for {
@@ -22,49 +33,67 @@ func (s *TRISA) AsyncHandler(stop <-chan struct{}) {
 		select {
 		case <-stop:
 			log.Info().Msg("asynchronous handler received stop signal")
+			cancel()
 			return
 		case <-ticker.C:
+			cancel()
 		}
 
-		log.Info().Msg("checking for pending transactions")
+		// Execute the handle async go routine with cancellation signals
+		log.Debug().Msg("checking for pending transactions and handling them")
+		ctx, cancel = context.WithTimeout(context.Background(), s.parent.conf.AsyncInterval)
+		go s.handleAsync(ctx)
+	}
+}
 
-		// Retrieve all pending messages from the database
-		var (
-			transactions []db.Transaction
-			err          error
-		)
-		if err = s.parent.db.LookupPending().Find(&transactions).Error; err != nil {
-			log.Error().Err(err).Msg("could not lookup transactions")
+func (s *TRISA) handleAsync(ctx context.Context) {
+	// Recover from panics that occur during async handling.
+	defer func() {
+		if r := recover(); r != nil {
+			log.WithLevel(zerolog.PanicLevel).
+				Err(fmt.Errorf("%v", r)).
+				Str("stack_trace", string(debug.Stack())).
+				Msg("handle async has recovered from a panic")
+		}
+	}()
+
+	// Retrieve all pending messages from the database
+	var (
+		transactions []db.Transaction
+		err          error
+	)
+	if err = s.parent.db.LookupPending().Find(&transactions).Error; err != nil {
+		log.Error().Err(err).Msg("could not lookup transactions")
+		return
+	}
+
+	now := time.Now()
+txloop:
+	for _, tx := range transactions {
+		// Verify pending transaction is old enough
+		if now.Before(tx.NotBefore) {
 			continue
 		}
 
-		now := time.Now()
-		for _, tx := range transactions {
-			// Verify pending transaction is old enough
-			if now.Before(tx.NotBefore) {
-				continue
-			}
-
-			// Verify pending transaction has not expired
-			if now.After(tx.NotAfter) {
-				log.Info().Uint("id", tx.ID).Time("not_after", tx.NotAfter).Msg("transaction expired")
-				tx.State = pb.TransactionState_EXPIRED
-				if err = s.parent.db.Save(&tx).Error; err != nil {
-					log.Error().Err(err).Uint("id", tx.ID).Msg("could not save expired transaction")
-				}
-				continue
-			}
-
-			// Acknowledge the transaction with the originator
-			if err = s.acknowledgeTransaction(&tx); err != nil {
-				log.Error().Err(err).Uint("id", tx.ID).Msg("could not acknowledge transaction")
-				tx.State = pb.TransactionState_FAILED
-			}
-
-			// Save the updated transaction in the database
+		// Verify pending transaction has not expired
+		if now.After(tx.NotAfter) {
+			log.Info().Uint("id", tx.ID).Time("not_after", tx.NotAfter).Msg("transaction expired")
+			tx.State = pb.TransactionState_EXPIRED
 			if err = s.parent.db.Save(&tx).Error; err != nil {
-				log.Error().Err(err).Uint("id", tx.ID).Msg("could not save completed transaction")
+				log.Error().Err(err).Uint("id", tx.ID).Msg("could not save expired transaction")
 			}
+			continue txloop
+		}
+
+		// Acknowledge the transaction with the originator
+		if err = s.acknowledgeTransaction(&tx); err != nil {
+			log.Error().Err(err).Uint("id", tx.ID).Msg("could not acknowledge transaction")
+			tx.State = pb.TransactionState_FAILED
+		}
+
+		// Save the updated transaction in the database
+		if err = s.parent.db.Save(&tx).Error; err != nil {
+			log.Error().Err(err).Uint("id", tx.ID).Msg("could not save completed transaction")
 		}
 	}
 }
