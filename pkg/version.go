25a5c623dc010cf3f4d7c9ebb872fc668585a37b
Patrick Deziel <deziel.patrick@gmail.com>
2022-08-15 15:03:20 -0500

    version bump to v1.0.4

--- pkg/version.go
+++ pkg/version.go
@@ -6,7 +6,7 @@ import "fmt"
 const (
 	VersionMajor         = 1
 	VersionMinor         = 0
-	VersionPatch         = 3
+	VersionPatch         = 4
 	VersionReleaseLevel  = ""
 	VersionReleaseNumber = 0
 )
